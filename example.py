0420b6da56565c9f87672198df640c69310e4d5d
Antonio <antonio.velasco@elastacloud.com>
2023-04-05 16:31:57 +0200

    Update to comply with python's coding standards

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
-from utils import CleanDuplicated, CleanSpaces, OnlyAlphanumeric
-from utils import LowerCase, Nums2Words
+from utils import cleanduplicated, cleanspaces, onlyalphanumeric
+from utils import lowercase, nums2words
 
 ## Examples on how to use the modular functions
 
-def Clean_Raw_text(text: str) -> str:
+def cleantext(text: str) -> str:
     text = CleanDuplicated(text)
     text = LowerCase(text)
     text = CleanSpaces(text)
@@ -12,7 +12,7 @@ def Clean_Raw_text(text: str) -> str:
     text = CleanDuplicated(text)
     return text
 
-def Clean_Cognitive_Search_Content(text: str) -> str: 
+def clean_cognitive_search_content(text: str) -> str: 
     text = ' '.join([t for t in text.split('\n') if len(t.split(' '))>1 and len(t)>3]) 
     text = CleanDuplicated(text) 
     text = OnlyAlphanumeric(text) 
--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,15 @@
-from itertools import chain
-import pandas as pd
 import re
-from num2words import num2words
+from itertools import chain
 
 import nltk
-from nltk.stem import WordNetLemmatizer, PorterStemmer
-from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+from num2words import num2words
+
+# ==============================================
+# ============  NLP  Processing  ===============
+# ==============================================
 
 #Download resources 
 nltk.download('punkt')
@@ -14,44 +17,41 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
-def CleanSpaces(input: str) -> str:
+def cleanspaces(input: str) -> str:
     # Clean multiple empty spaces
     regex = r'(\s\s+)'
     return re.sub(regex, r' ', input, flags=re.IGNORECASE)
     
-def CleanDuplicated(input: str) -> str:
+def cleanduplicated(input: str) -> str:
     # Matches repeated words and sentences
     regex = r'(\W|^)(.+)\s\2'
     return re.sub(regex, r'\1', input, flags=re.IGNORECASE)
     
-def OnlyAlphabetic(input: str) -> str:
+def onlyalphabetic(input: str) -> str:
     # Matches every non alphabetical character
     regex = '[^a-zA-Z ]'
     return re.sub(regex , " ", input)
 
-def OnlyAlphanumeric(input: str) -> str:
+def onlyalphanumeric(input: str) -> str:
     # Matches every non alphanumerical character
     regex = '[^a-zA-Z1-9 ]'
     return re.sub(regex, " ", input)
 
-def LowerCase(input: str) -> str:
+def lowercase(input: str) -> str:
     return input.lower()
 
-def Nums2Words(input: str) -> str:
+def nums2words(input: str) -> str:
     regex = ' \d\.?\d* '
     return re.sub(regex, lambda x: ' ' + num2words(x.group()) + ' ', input)
 
-def RemoveStopWords(input: list[str], *args: str) -> list[str]:
-
+def removestopwords(input: list[str], *args: str) -> list[str]:
     # Removes common English stopwords and custom ones added as *args 
     return [t for t in input if t not in list(chain(*[[*args], stopwords.words('english')]))]
 
-def Lemmatizer(input: list[str]) -> list[str]:
-
+def lemmatizer(input: list[str]) -> list[str]:
     # Lemmatizes tokens
     return [WordNetLemmatizer().lemmatize(t) for t in input]
 
-def Stemmer(input: list[str]) -> list[str]:
-
+def stemmer(input: list[str]) -> list[str]:
     # Lemmatizes tokens
     return [PorterStemmer().stem(t) for t in input]
